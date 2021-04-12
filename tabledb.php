a1d953b4f30d1b4d5ca88cbdfee5504140593be6
davidpitutiu <dpitutiu@icloud.com>
2021-04-12 12:14:52 +0300

    Update tabledb.php

--- tabledb.php
+++ tabledb.php
@@ -3,19 +3,18 @@
     $username = 'root';
     $password = '';
     $connection=mysqli_connect($hostname,$username,$password);
-    $table="CREATE TABLE alphacrm.tCompany(";
-    $table.="ID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, ";
-    $table.="preName VARCHAR(50) , ";
-    $table.="Name VARCHAR(250) NOT NULL, ";
-    $table.="RegType VARCHAR(50) NULL, ";
-    $table.="StreetA VARCHAR(150) NULL, ";
-    $table.="StreetB VARCHAR(150) NULL, ";
-    $table.="StreetC VARCHAR(150) NULL, ";
-    $table.="Town VARCHAR(150) NULL, ";
-    $table.="Country VARCHAR(150) NULL, ";
-    $table.="Postcode VARCHAR(50) NULL, ";
-    $table.="COUNTRY VARCHAR(250) NOT NULL";
-    $table.=")";
+    $table="CREATE TABLE alphacrm.tCompany(
+        ID INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+        preName VARCHAR(50) NOT NULL, 
+        firstName VARCHAR(250) NOT NULL, 
+        RegType VARCHAR(50) NULL, 
+        StreetA VARCHAR(150) NULL, 
+        StreetB VARCHAR(150) NULL, 
+        StreetC VARCHAR(150) NULL, 
+        Town VARCHAR(150) NULL, 
+        Country VARCHAR(150) NULL, 
+        Postcode VARCHAR(50) NULL, )";
+    echo $table;
     $create=mysqli_query($connection,$table);
     if($create){
         echo"OK";
