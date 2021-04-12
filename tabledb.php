04e7cba5bd5a812ae18eda51ddde419a49ec916f
davidpitutiu <dpitutiu@icloud.com>
2021-04-12 21:30:26 +0300

    Update tabledb.php

--- tabledb.php
+++ tabledb.php
@@ -5,19 +5,33 @@
     $connection=mysqli_connect($hostname,$username,$password);
     $table="CREATE TABLE alphacrm.tCompany(
         ID INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-        preName VARCHAR(50) NOT NULL, 
-        firstName VARCHAR(250) NOT NULL, 
+        preName VARCHAR(50) , 
+        Name VARCHAR(250) NOT NULL, 
         RegType VARCHAR(50) NULL, 
         StreetA VARCHAR(150) NULL, 
         StreetB VARCHAR(150) NULL, 
         StreetC VARCHAR(150) NULL, 
         Town VARCHAR(150) NULL, 
-        Country VARCHAR(150) NULL, 
-        Postcode VARCHAR(50) NULL, )";
-    echo $table;
+        County VARCHAR(150) NULL, 
+        Postcode VARCHAR(50) NULL,
+        COUNTRY VARCHAR(250) NOT NULL)";
     $create=mysqli_query($connection,$table);
     if($create){
         echo"OK";
+    }else{
+        echo "Failed";
+    }
+    $persontable="CREATE TABLE alphacrm.tPerson(
+        ID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        Salutation VARCHAR(20) ,
+        FirstName VARCHAR(50) ,
+        LastName VARCHAR(50) NOT NULL , 
+        CompanyID INT(11) NOT NULL )";
+    $create2=mysqli_query($connection,$persontable);
+    echo "<br/>";
+    if($create2)
+    {
+        echo "OK";
     }else{
         echo "Failed";
     }
